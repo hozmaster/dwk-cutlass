d7ed13fcc987f2b84a31d63c6f8d20f08df25558
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2026-01-29 15:01:45 +0200

    Start work with backup

---
+++ cutlass-backup/cutbackup.sh
@@ -0,0 +1,9 @@
+set -e
+
+if [ -n "$URL" ]
+then
+  PG_URL="${URL/DB_PASSWORD/$DB_PASSWORD}"
+  pg_dump -v "$PG_URL" > /usr/src/app/backup.sql
+  echo "Not sending the dump actually anywhere"
+  # curl -F ‘data=@/usr/src/app/backup.sql’ https://somewhere
+fi
