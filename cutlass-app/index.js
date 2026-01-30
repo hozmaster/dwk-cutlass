abf9ddcfe9723defae07e510a9af3104605ee21a
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2026-01-30 10:21:26 +0200

    Add comment to code (trigger the build?)

--- cutlass-app/index.js
+++ cutlass-app/index.js
@@ -2,9 +2,11 @@ const app = require('./src/app');
 const {initSystem} = require("./src/library/image");
 const PORT = process.env.APP_PORT || 3000;
 
+// Start listening incoming requests
 app.listen(PORT, () => {
     initSystem().then(() => {
             console.log("Server started in port " + PORT);
         }
     );
 });
+
