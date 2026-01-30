aeae93119cb33cf1662447afed0d062102a925db
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2026-01-30 11:02:34 +0200

    Trigger builkd process

--- cutlass-app/index.js
+++ cutlass-app/index.js
@@ -2,9 +2,12 @@ const app = require('./src/app');
 const {initSystem} = require("./src/library/image");
 const PORT = process.env.APP_PORT || 3000;
 
+// Update
+
 app.listen(PORT, () => {
     initSystem().then(() => {
             console.log("Server started in port " + PORT);
         }
     );
 });
+
