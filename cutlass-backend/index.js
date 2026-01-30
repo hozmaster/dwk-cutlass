0b603be3398ce8c60835dd7a51048e0ea33c69e0
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2026-01-30 10:32:47 +0200

    Add more comments

--- cutlass-backend/index.js
+++ cutlass-backend/index.js
@@ -2,6 +2,7 @@ const app = require('./src/app');
 const {setupDatabase} = require("./src/db/database");
 const PORT = process.env.BACKEND_PORT || 3010;
 
+// start to listen incoming requests
 app.listen(PORT, () => {
     setupDatabase().then(() => {
         console.log("The todo started in port " + PORT);
